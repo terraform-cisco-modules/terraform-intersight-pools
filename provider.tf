3c81f9ae069ec96d49c68530ad63523792326fa6
tyscott@cisco.com <tyscott@cisco.com>
2024-08-05 11:37:32 -0400

    Updated to provider 1.0.51

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     intersight = {
       source  = "CiscoDevNet/intersight"
-      version = ">=1.0.50"
+      version = ">=1.0.51"
     }
   }
   required_version = ">=1.3.0"
